30a0b3502a4e3cbf44382cc8efbcaa13d9b4a4b3
MarcosSouzaa <marcossouza.git@gmail.com>
2023-11-02 14:44:36 -0300

    Criando o diretorio Template e utilizando render para chamar o html.

--- jogoteca.py
+++ jogoteca.py
@@ -2,12 +2,12 @@
 # flask: referencio à Biblioteca flask e Flask: importando a função Flask para a construção da aplicação
 # o flask não é uma função embutida do python, vou ter que importar no terminal com PiP
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return '<h1>Olá Mundo!</h1>'
+    return render_template('lista.html')
 
 app.run()
